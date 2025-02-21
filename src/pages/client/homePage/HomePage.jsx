a411d27c6677ed2ba60ba328ca24a451c7969bf7
ashikur-ai <projectshortlife@gmail.com>
2025-02-21 20:56:49 +0600

    working on sales section

--- src/pages/client/homePage/HomePage.jsx
+++ src/pages/client/homePage/HomePage.jsx
@@ -4,6 +4,7 @@ import KeyPoints from './homeComponents/KeyPoints';
 import PopularCategory from './homeComponents/PopularCategory';
 import PopularProduct from './homeComponents/PopularProduct';
 import HotDeals from './homeComponents/HotDeals';
+import SaleSection from './homeComponents/SaleSection';
 
 const HomePage = () => {
   return (
@@ -19,6 +20,10 @@ const HomePage = () => {
 
       <div className="mt-20">
         <HotDeals />
+      </div>
+
+      <div className="mt-20">
+        <SaleSection/>
       </div>
     </div>
   );
